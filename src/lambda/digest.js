2940df41b6e9da822f50d340a526d7d8cc3919d4
unknown <nicoletobe2000@gmail.com>
2020-04-03 20:05:50 +0400

    better auth validation

--- src/lambda/digest.js
+++ src/lambda/digest.js
@@ -9,7 +9,8 @@ exports.handler = async (event, context)=>{
              if(event.httpMethod != 'POST') return {statusCode: 404}
              //404 code for no reveal(401 works too but reveals that the endpoint exists, our cron job will 
              //always have its auth header set so we can safely assume any access w/o it is someone else.
-             if(!event.headers.authorization !== process.env.AUTH_KEY)  return {statusCode: 404}
+             if(!event.headers.authorization)  return {statusCode: 404};
+             if(event.headers.authorization.split(' ')[1] !== process.env.AUTH_KEY) return {statusCode: 404};
              //get tweets
              const tweets = await twitter();
              //send email if there are tweets available
