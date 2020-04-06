473f0c550999cdc76c7ec138a82d966fa85c563e
unknown <nicoletobe2000@gmail.com>
2020-04-06 12:51:31 +0400

    improved look of email a tad bit

--- src/utils/email.js
+++ src/utils/email.js
@@ -4,15 +4,15 @@ module.exports = (tweets) => {
     //insert tweet data into elements
     tweets.forEach(tweet=>{
        tweetElements += `
-                <div style = "width: 60%; border: 1px solid black; margin-bottom:5px; padding: 2px ">
+                <div style = "width: 100%; border: 1px solid black; margin-bottom:10px; padding: 10px ">
                 <p>@${tweet.user}</p>
                 <p>${tweet.text}</p>
-                <a style="color: black; text-decoration:none" href="${tweet.url}">Read Tweet => </a>
+                <a style="color: white; padding:5px; background: black; text-decoration:none" href="${tweet.url}">Read Tweet </a>
                 </div>
                `
     })
     let email = `   
-        <div style = "width: 600px;">
+        <div style = "width: 40rem; font-size: 1.2rem; margin: 0 auto">
             ${tweetElements}
         </div>
     
